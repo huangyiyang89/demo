ba67a6ccf46c5e0d6108664a85377223ae00fb68
Huang <418618674@qq.com>
2024-08-03 13:46:10 +0800

    replay fix

--- src/page/Replay.jsx
+++ src/page/Replay.jsx
@@ -1,11 +1,19 @@
 // src/EventVideoPlayer.js
-import { useState, useRef } from "react";
-import { Layout, Select, DatePicker, Button, Flex } from "antd";
+import { useState, useRef, useEffect } from "react";
+import {
+  Layout,
+  Select,
+  DatePicker,
+  Button,
+  Flex,
+  Typography,
+  message,
+} from "antd";
 import "antd/dist/reset.css";
 import FlvPlayer from "../component/FlvPlayer";
-
+import axios from "axios";
+const { Title } = Typography;
 const { Sider, Content } = Layout;
-
 
 const Replay = () => {
   const [selectedCamera, setSelectedCamera] = useState("");
@@ -13,59 +21,73 @@ const Replay = () => {
   const [cameras, setCameras] = useState([]);
   const playerRef = useRef(null);
 
-
   const handleDateChange = (date, dateString) => {
     setSelectedDate(dateString);
   };
 
-  const handleReplayClick = () => {
-    console.log("Replay video for date:", selectedDate);
-    if (selectedDate) {
-      console.log("Replay video for date:", selectedDate);
-      if (playerRef.current) {
-        console.log("play");
-        playerRef.current.play();
+  const fetchCameras = async () => {
+    try {
+      const response = await axios.get("/api/cameras/");
+      const cameras = response.data;
+      console.log("cameras:", cameras);
+      setCameras(cameras);
+    } catch (error) {
+      if (error.response) {
+        message.error(error.response.data.detail);
+      } else {
+        message.error(error.message);
       }
     }
   };
 
+  useEffect(() => {
+    fetchCameras();
+  }, []);
   return (
-    <Layout>
-      <Sider width={300} style={{ background: "#fff", padding: "20px" }}>
-        <Flex gap="small" vertical>
-          <h4>选择摄像机</h4>
-          <Select
-            options={cameras.map((camera) => ({
-              value: camera.id,
-              label: camera.name,
-            }))}
-            placeholder="选择摄像机"
-            onChange={setSelectedCamera}
-            onClick={setSelectedCamera}
-          />
-          <h4 style={{ marginTop: "20px" }}>选择日期</h4>
-          <DatePicker
-            showTime={{
-              format: "HH:mm",
-            }}
-            format="YYYY-MM-DD HH:mm"
-            onChange={(value, dateString) => {
-              console.log("Selected Time: ", value);
-              console.log("Formatted Selected Time: ", dateString);
-            }}
-            onOk={handleDateChange}
-          />
-          <Button
-            type="primary"
-            style={{ marginTop: "20px" }}
-            onClick={handleReplayClick}
-          >
-            回放
-          </Button>
-        </Flex>
-      </Sider>
-      <Content>
-      <div
+    <div>
+      <Title level={4}>录像回放</Title>
+
+      <Layout>
+        <Sider width={400} style={{ background: "#fff", padding: "20px" }}>
+          <Flex gap="small" vertical>
+            <h4>选择摄像机</h4>
+            <Select
+              options={cameras.map((camera) => ({
+                value: camera.id,
+                label: camera.name,
+              }))}
+              placeholder="选择摄像机"
+              onChange={(e) => {
+                const selectedCamera = cameras.find(
+                  (camera) => camera.id === e
+                );
+                setSelectedCamera(selectedCamera);
+              }}
+              value={selectedCamera?.name}
+            ></Select>
+            <h4 style={{ marginTop: "20px" }}>选择时间</h4>
+            <DatePicker
+              showTime={{
+                format: "HH:mm",
+              }}
+              format="YYYY-MM-DD HH:mm"
+              onChange={(value, dateString) => {
+                console.log("Selected Time: ", value);
+                console.log("Formatted Selected Time: ", dateString);
+              }}
+              onOk={handleDateChange}
+            />
+            <Button
+              type="primary"
+              style={{ marginTop: "20px" }}
+              onClick={() => {}}
+            >
+              回放
+            </Button>
+          </Flex>
+        </Sider>
+        <Content>
+          <div
             style={{
               width: "100%",
               position: "relative",
@@ -73,10 +95,11 @@ const Replay = () => {
               height: 0,
             }}
           >
-            <FlvPlayer camera={selectedCamera}></FlvPlayer>
+            <FlvPlayer url={selectedCamera?.ip_addr}></FlvPlayer>
           </div>
-      </Content>
-    </Layout>
+        </Content>
+      </Layout>
+    </div>
   );
 };
 export default Replay;
